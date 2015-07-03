f12f8d1158141f10ad750bd7e83d9c9265d1b184
GrandCircusPaul <paul@grandcircus.com>
2015-07-03 01:59:29 -0400

    Added console output
    Added "Hello, World' syntax
    Commented-out above
    Added code to greet passed in argument.  For instance if a
    "NameOfPerson" is passed as an argument (e.g., calling HelloWorld.exe
    from the command line with arguments), then the program outputs "Hello,
    NameOfPerson."

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,6 +10,11 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            //System.Console.WriteLine("Hello, World!");
+            //System.Console.WriteLine(args[0]);
+            System.Console.Write("Hello, ");
+            System.Console.Write(args[0]);
+            System.Console.WriteLine("!");
         }
     }
 }
